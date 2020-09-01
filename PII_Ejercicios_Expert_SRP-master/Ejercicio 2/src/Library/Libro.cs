f1131238d877ab74301b2d2276bb07a2c2be313a
Nicolas Texidor <NICOLAS.TEXIDOR@correo.ucu.edu.uy>
2020-09-01 20:29:32 -0300

    comentarios ej2

--- PII_Ejercicios_Expert_SRP-master/Ejercicio 2/src/Library/Libro.cs
+++ PII_Ejercicios_Expert_SRP-master/Ejercicio 2/src/Library/Libro.cs
@@ -1,5 +1,8 @@
 ﻿using System;
-
+//Creo que esta clase conoce demasiadas cosas y tiene más responsabilidades de las necesarias.
+//Se podría crear otra clase que sepa únicamente el SectorBiblioteca y el EstanteBiblioteca. 
+//Además de esto, dicha clase debería tener la responsabilidad de AlmacenarLibro en lugar de esta.
+//De esta forma si el libro cambiase de estante o de sector se podría cambiar fácilmente accediendo a la otra clase en lugar de entrar a esta que está llena de otra info.
 namespace SRP
 {
     public class Libro
